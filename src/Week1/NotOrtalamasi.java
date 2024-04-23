449a50d96561ac9a61a2578cde4f07459f2aa30c
Onur Mergen <mergenonur@gmail.com>
2024-04-23 16:17:00 +0300

    Initial commit

---
+++ src/Week1/NotOrtalamasi.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+
+        int Matamatik,Fizik,Kimya,Biyoloji,Turkce,Tarih,Muzik ;
+
+        Scanner NotOrtalamasi = new Scanner(System.in);
+
+        System.out.print("Matamatik Notunuz : ") ;
+        Matamatik = NotOrtalamasi.nextInt() ;
+
+        System.out.print("Fizik Notunuz : ") ;
+        Fizik =  NotOrtalamasi.nextInt() ;
+
+        System.out.print("Kimya Notunuz : ") ;
+        Kimya =  NotOrtalamasi.nextInt() ;
+
+        System.out.print("Biyoloji Notunuz : ") ;
+        Biyoloji = NotOrtalamasi.nextInt() ;
+
+        System.out.print("Türkçe Notunuz : ") ;
+        Turkce =  NotOrtalamasi.nextInt() ;
+
+        System.out.print("Tarih Notunuz : ") ;
+        Tarih =  NotOrtalamasi.nextInt() ;
+
+        System.out.print("Muzik Notunuz : ") ;
+        Muzik = NotOrtalamasi.nextInt() ;
+
+        double ortalama =  (Matamatik+Fizik+Kimya+Biyoloji+Turkce+Tarih+Muzik) / 7 ;
+        String sonuc = (ortalama >= 60) ? "Geçtiniz" : "Kaldınız" ;
+        System.out.println("7 Dersin Ortalama Puanı : " + ortalama);
+        System.out.println("Sonuç : " + sonuc);
+
+    }
+}
