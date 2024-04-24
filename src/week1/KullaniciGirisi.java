f238d4d217dd9baa3d135e3f7cc859ed4f50b8e4
Onur Mergen <mergenonur@gmail.com>
2024-04-25 00:22:08 +0300

    Güncelledim

--- src/week1/KullaniciGirisi.java
+++ src/week1/KullaniciGirisi.java
@@ -13,25 +13,31 @@ public class KullaniciGirisi {
         System.out.print("Şifreniz : ");
         Password = profil.nextLine();
 
+
+        /* Sizin Yerinize Belirlemiş olduğum Doğru Kullanıcı Adınız : Patika+
+                                                   Şifreniz : Java101 */
+
+
         if (Username.equals("Patika+") && Password.equals("Java101")) {
             System.out.println("Giriş Başarılı");
 
         } else {
             System.out.println("Hatalı Şifre!!! Şifrenizi Unuttuysanız Sıfırlamak İstermisiniz? ");
             System.out.println("Evet\nHayır");
-            Cevap = profil.nextLine();
-            //Verilecek olan yanıtı (Evet Ve Hayır) seçebilmemiz için bu satırı buraya ekledim
+            Cevap = profil.nextLine(); //Verilecek olan yanıtı (Evet Ve Hayır) seçebilmemiz için bu satırı buraya ekledim.
             if (Cevap.equals("Evet")) {
-                System.out.println("Yeni Şifrenizi Giriniz : ");
+                System.out.print("Yeni Şifrenizi Giriniz : ");
                 String NewPassword = profil.nextLine();
                 if (NewPassword.equals(Password)) {
                     System.out.println("Lütfen Eskisinden Farklı Bir Şifre Giriniz : ");
                 } else {
                     System.out.println("Yeni Şifreniz Oluşturuldu.");
                 }
-            }
-            if (Cevap.equals("Hayır")) {
+            } else if (Cevap.equals("Hayır")) {
                 System.out.println("Tekrar Bekleriz.");
+
+            } else {
+                System.out.println("Hatalı Giriş Yaptınız!!!");
 
             }
         }
