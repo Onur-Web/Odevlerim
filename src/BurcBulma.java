69126005548fbbb22a9932c6e20276b4e1846650
Onur Mergen <mergenonur@gmail.com>
2024-04-25 15:03:19 +0300

    Ödev Teslimi

---
+++ src/BurcBulma.java
@@ -0,0 +1,148 @@
+import java.util.Scanner;
+
+public class BurcBulma {
+    public static void main(String[] args) {
+
+        int ay, gun;
+        String burc = "";
+        boolean isYanlis = false;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğduğunuz Ay : ");
+        ay = input.nextInt();
+
+        System.out.print("Doğduğunuz Gün : ");
+        gun = input.nextInt();
+
+        if ((ay >= 1) && (ay <= 12)) {
+            if (ay == 1) {
+                if (gun >= 1 && gun <= 31) {
+                    if (gun < 22) {
+                        burc = "Oğlak";
+                    } else {
+                        burc = "Kova";
+                    }
+                } else {
+                    isYanlis = true;
+                }
+            } else if (ay == 2) {
+                if (gun >= 1 && gun <= 29) {
+                    if (gun < 20) {
+                        burc = "Kova";
+                    } else {
+                        burc = "Balık";
+                    }
+                } else {
+                    isYanlis = true;
+                }
+            } else if (ay == 3) {
+                if (gun >= 1 && gun <= 31) {
+                    if (gun < 21) {
+                        burc = "Balık";
+                    } else {
+                        burc = "Koç";
+                    }
+                } else {
+                    isYanlis = true;
+                }
+            } else if (ay == 4) {
+                if (gun >= 1 && gun <= 30) {
+                    if (gun < 21) {
+                        burc = "Koç";
+                    } else {
+                        burc = "Boğa";
+                    }
+                } else {
+                    isYanlis = true;
+                }
+            } else if (ay == 5) {
+                if (gun >= 1 && gun <= 31) {
+                    if (gun < 22) {
+                        burc = "Boğa";
+                    } else {
+                        burc = "İkizler";
+                    }
+                } else {
+                    isYanlis = true;
+                }
+            } else if (ay == 6) {
+                if (gun >= 1 && gun <= 30) {
+                    if (gun < 23) {
+                        burc = "İkizler";
+                    } else {
+                        burc = "Yengeç";
+                    }
+                } else {
+                    isYanlis = true;
+                }
+            } else if (ay == 7) {
+                if (gun >= 1 && gun <= 31) {
+                    if (gun < 23) {
+                        burc = "Yengeç";
+                    } else {
+                        burc = "Aslan";
+                    }
+                } else {
+                    isYanlis = true;
+                }
+            } else if (ay == 8) {
+                if (gun >= 1 && gun <= 31) {
+                    if (gun < 23) {
+                        burc = "Aslan";
+                    } else {
+                        burc = "Başak";
+                    }
+                } else {
+                    isYanlis = true;
+                }
+            } else if (ay == 9) {
+                if (gun >= 1 && gun <= 30) {
+                    if (gun < 23) {
+                        burc = "Başak";
+                    } else {
+                        burc = "Terazi";
+                    }
+                } else {
+                    isYanlis = true;
+                }
+            } else if (ay == 10) {
+                if (gun >= 1 && gun <= 31) {
+                    if (gun < 23) {
+                        burc = "Terazi";
+                    } else {
+                        burc = "Akrep";
+                    }
+                } else {
+                    isYanlis = true;
+                }
+            } else if (ay == 11) {
+                if (gun >= 1 && gun <= 30) {
+                    if (gun < 22) {
+                        burc = "Akrep";
+                    } else {
+                        burc = "Yay";
+                    }
+                } else {
+                    isYanlis = true;
+                }
+            } else if (ay == 12) {
+                if (gun >= 1 && gun <= 31) {
+                    if (gun < 22) {
+                        burc = "Yay";
+                    } else {
+                        burc = "Oğlak";
+                    }
+                } else {
+                    isYanlis = true;
+                }
+            }
+            if (isYanlis) {
+                System.out.println("Hatalı Giriş Yaptınız!!!");
+            } else {
+                System.out.println("Burcunuz : " + burc);
+            }
+        }
+
+    }
+}
--- src/week1/SinifGecmeDurumu.java
+++ src/week1/SinifGecmeDurumu.java
@@ -5,42 +5,43 @@ import java.util.Scanner;
 public class SinifGecmeDurumu {
     public static void main(String[] args) {
 
-        int matamatik, fizik, kimya, turkce, muzik;
-        int toplamNot = 0, dersSayisi = 0;
-        int ortalama;
+        double matamatik, fizik, kimya, turkce, muzik;
+        double toplamNot = 0;
+        int dersSayisi = 0;
+        double ortalama;
 
         Scanner sinif = new Scanner(System.in);
 
         System.out.print("Matamatik Notunuz : ");
-        matamatik = sinif.nextInt();
+        matamatik = sinif.nextDouble();
         if (matamatik >= 0 && matamatik <= 100) {
             toplamNot += matamatik;
             dersSayisi++;
         }
 
         System.out.print("Fizik Notunuz : ");
-        fizik = sinif.nextInt();
+        fizik = sinif.nextDouble();
         if (fizik >= 0 && fizik <= 100) {
             toplamNot += fizik;
             dersSayisi++;
         }
 
         System.out.print("Kimya Notunuz : ");
-        kimya = sinif.nextInt();
+        kimya = sinif.nextDouble();
         if (kimya >= 0 && kimya <= 100) {
             toplamNot += kimya;
             dersSayisi++;
         }
 
         System.out.print("Türkçe Notunuz : ");
-        turkce = sinif.nextInt();
+        turkce = sinif.nextDouble();
         if (turkce >= 0 && turkce <= 100) {
             toplamNot += turkce;
             dersSayisi++;
         }
 
         System.out.print("Müzik Notunuz : ");
-        muzik = sinif.nextInt();
+        muzik = sinif.nextDouble();
         if (muzik >= 0 && muzik <= 100) {
             toplamNot += muzik;
             dersSayisi++;
