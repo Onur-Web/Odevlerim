99876404bb83f92b0babb43b9e3dd1d6de6cbbea
Onur Mergen <mergenonur@gmail.com>
2024-04-27 00:18:53 +0300

    Ödev

---
+++ src/week1/TekSayilarinToplami.java
@@ -0,0 +1,21 @@
+package week1;
+
+import java.util.Scanner;
+
+public class TekSayilarinToplami {
+    public static void main(String[] args) {
+        Scanner sawMe = new Scanner(System.in);
+
+        int sayi, total = 0;
+
+        do {
+            System.out.print("Sayı Giriniz : ");
+            sayi = sawMe.nextInt();
+            if (sayi % 4 == 0 ) {
+                total += sayi;
+            }
+        } while (sayi % 2 == 0);
+        System.out.println("Toplam : " + total);
+
+    }
+}
