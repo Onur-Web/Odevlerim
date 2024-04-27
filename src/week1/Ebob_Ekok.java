2ded5b6c6a1308ac53ccd05eae2ec85158c8a3ec
Onur Mergen <mergenonur@gmail.com>
2024-04-27 21:20:23 +0300

    Ödev Teslim

---
+++ src/week1/Ebob_Ekok.java
@@ -0,0 +1,37 @@
+package week1;
+
+import java.util.Scanner;
+
+public class Ebob_Ekok {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("(n1) Sayısını Giriniz : ");
+        int n1 = input.nextInt();
+        System.out.print("(n2) Sayısını Giriniz : ");
+        int n2 = input.nextInt();
+
+        int minimum;
+        if (n1 < n2) {
+            minimum = n1;
+        } else {
+            minimum = n2;
+        }
+
+        //EKOK = (n1*n2) / EBOB
+
+        int ebob = 1;
+        int i = 1;
+        while (i <= minimum) {
+            if ((n1 % i == 0 && n2 % i == 0)) {
+                ebob = i;
+            }
+            i++;
+
+        }
+        System.out.println("Sayıların EBOB'u : " + ebob);
+
+        int ekok = n1 * n2 / ebob;
+        System.out.println("Sayıların EKOK'u : " + ekok);
+    }
+}
