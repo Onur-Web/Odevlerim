178fedaad1b5226ffe53b54e295ecf36a8c9768d
Onur Mergen <mergenonur@gmail.com>
2024-04-28 13:48:48 +0300

    Tamirat1

--- src/week1/FlyTicketPrice.java
+++ src/week1/FlyTicketPrice.java
@@ -10,33 +10,29 @@ public class FlyTicketPrice {
         int mesafe = input.nextInt();
         System.out.print("Yaşınızı Giriniz : ");
         int yas = input.nextInt();
-        System.out.print("Yolculuk Tipini Giriniz (1: Tek Yön \" + \"2: Gidiş-Dönüş) : ");
+        System.out.print("Yolculuk Tipini Giriniz (1: Tek Yön, 2: Gidiş-Dönüş) : ");
         int tip = input.nextInt();
 
         double perKM = 0.10;
         double toplam = mesafe * perKM;
 
-        if (mesafe <= 0 || yas <= 0 || tip <= 0) {
+        if (mesafe <= 0 || yas <= 0 || (tip != 1 && tip != 2)) {
             System.out.println("Hatalı Giriş Yaptınız!!!");
             return;
         }
 
-        if (yas < 12 ) {
-            toplam *= 0.5;
-        } else if (yas > 12 && yas <= 24) {
-            toplam *= 0.9;
+        if (yas < 12) {
+            toplam *= 0.5; // %50
+        } else if (yas >= 12 && yas <= 24) {
+            toplam *= 0.9; // %10
         } else if (yas >= 65) {
-            toplam *= 0.7;
-        }else {
-            toplam = 1.0;
+            toplam *= 0.7; // %30
         }
 
         if (tip == 2) {
             toplam *= 0.8;
-        }else {
-            toplam *= 1.0;
         }
 
-        System.out.println("Bilet Fiyatı : " + toplam + " $");
+        System.out.println("Bilet Fiyatı : " + toplam + " TL");
     }
 }
