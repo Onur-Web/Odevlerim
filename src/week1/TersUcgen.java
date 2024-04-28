22da951a8a58da40f8997792831d99870f72ab51
Onur Mergen <mergenonur@gmail.com>
2024-04-28 14:24:58 +0300

    Güncelleme

--- src/week1/TersUcgen.java
+++ src/week1/TersUcgen.java
@@ -2,18 +2,19 @@ package week1;
 
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class TersUcgen {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Basamak Sayısını Giriniz : ");
+        System.out.print("Basamak Sayısı: ");
         int n = scanner.nextInt();
         // Girilen Değer "n"
 
-
-        //Yıldız Sayısı 2 * n - 1
-        //Satır Sayısı = Girilen Değer
-        for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= i * 2 - 1; j++) {
+        // Yıldız Sayısı = Girilen Değer
+        // Satır Sayısı = Girilen Değer
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
