3ac0883bf3d11f4fae02c1d02691d14163a95952
Onur Mergen <mergenonur@gmail.com>
2024-04-28 14:16:04 +0300

    Teslim

---
+++ src/week1/TersUcgen.java
@@ -0,0 +1,22 @@
+package week1;
+
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Basamak Sayısı: ");
+        int n = scanner.nextInt();
+        // Girilen Değer "n"
+
+
+        //Yıldız Sayısı 2 * n - 1
+        //Satır Sayısı = Girilen Değer
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
