3dc03480861ff746af5f1262a69f8d2e7c331a9d
Onur Mergen <mergenonur@gmail.com>
2024-04-30 14:11:16 +0300

    Güncelleme3

--- src/week1/TersUcgen.java
+++ src/week1/TersUcgen.java
@@ -13,7 +13,7 @@ public class TersUcgen {
         // Satır Sayısı = Girilen Değer
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i + 1; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
