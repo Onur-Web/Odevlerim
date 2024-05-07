0d19b15acefaf5467163251a088e2b45af0673f3
Onur Mergen <mergenonur@gmail.com>
2024-05-07 13:00:28 +0300

    Güncelleme

---
+++ src/week2/DiziElemanFrekans.java
@@ -0,0 +1,49 @@
+package week2;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DiziElemanFrekans {
+
+    public static void main(String[] args) {
+        int[] array = {10, 20, 20, 10, 10, 20, 5, 20};
+        System.out.println("Dizi : " + Arrays.toString(array));
+
+//daha görmedik ama bir arkadaşım aracılığı ile öğrendim ve çok hoşuma gitti oyüzden kullanmak istedim
+        Map<Integer, Integer> myMap = new HashMap<>();
+        // for loop
+        for (int i = 0; i < array.length; i++) {
+            // arrayden elemanı oku
+            int number = array[i];
+            // map içine ekle
+            // eleman daha önceden bulundu mu?
+            if (myMap.get(number) == null) {
+                myMap.put(number, 1); // 10 --> 3
+            } else {
+                // bu eleman daha önceden bir defa kayıt edilmiş.
+                Integer counter = myMap.get(number);
+                counter++;
+                myMap.put(number, counter);
+            }
+        }
+        System.out.println("Tekrar Sayıları");
+        for (Map.Entry<Integer, Integer> item : myMap.entrySet()) {
+            System.out.println(item.getKey() + " sayısı " + item.getValue() + " kere tekrar edildi.");
+        }
+        // alternatif ve aslında doğru kullanımı
+        // for-each loop
+       /*
+       for (int number : array) {
+            if (myMap.get(number) == null) {
+                myMap.put(number, 1); // 10 --> 3
+            } else {
+                // bu eleman daha önceden bir defa kayıt edilmiş.
+                Integer counter = myMap.get(number);
+                counter++;
+                myMap.put(number, counter);
+            }
+        }
+        */
+    }
+}
---
+++ src/week2/MatrisTranspoz.java
@@ -0,0 +1,38 @@
+package week2;
+
+public class MatrisTranspoz {
+
+    public static void main(String[] args) {
+        int[][] originalArray = {{2, 3, 4}, {5, 6, 4}};
+        // kaç satır
+        // kaç sutn var
+        // 3x3 --> 9 input iste
+
+        // Orijinal matrisi yazdır
+        System.out.println("Matris:");
+        printMatrix(originalArray);
+
+        int rowCount = originalArray.length;
+        int columnCount = originalArray[0].length;
+
+        int[][] transposeArray = new int[columnCount][rowCount];
+        // Transpoz matrisi hesapla
+        for (int row = 0; row < rowCount; row++) {
+            for (int column = 0; column < columnCount; column++) {
+                transposeArray[column][row] = originalArray[row][column];
+            }
+        }
+        // Transpoz yazdırma
+        System.out.println("Transpoze:");
+        printMatrix(transposeArray);
+    }
+    // Matris yazdırma
+    public static void printMatrix(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[0].length; column++) {
+                System.out.print(matrix[row][column] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
