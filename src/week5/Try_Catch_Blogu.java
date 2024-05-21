7e46ddf953f43b09ce8ba72ac0313646b50d2bf4
Onur Mergen <mergenonur@gmail.com>
2024-05-21 16:40:22 +0300

    Ödev

--- src/week5/Try_Catch_Blogu.java
+++ src/week5/Try_Catch_Blogu.java
@@ -1,14 +1,11 @@
 package week5;
-
 import java.util.Scanner;
-
 public class Try_Catch_Blogu {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Lütfen ((0 ve 10 dahil) arasında) bir indeks girin: ");
         int index = scanner.nextInt();
-
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         try {
             int eleman = getElement(array, index);
@@ -17,7 +14,6 @@ public class Try_Catch_Blogu {
             System.out.println("Hata: Belirtilen indeks dizi boyutunun dışında!");
         }
     }
-
     public static int getElement(int[] array, int index) {
         return array[index];
     }
